17e4b59d81a0be9019a5cadf6128764d416ceae3
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-25 23:07:05 -0300

    feat: creates use case to register a user

---
+++ src/use-cases/user/register.ts
@@ -0,0 +1,31 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcrypt'
+
+export interface RegisterUserUseCaseParams {
+  name: string
+  email: string
+  password: string
+}
+export class Register {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUserUseCaseParams) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (!userAlreadyExists) {
+      throw new Error()
+    }
+
+    const passwordHash = await hash(password, 'dailyDietApi')
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash: passwordHash,
+    })
+
+    return {
+      user: { ...user, passwordHash: undefined },
+    }
+  }
+}
