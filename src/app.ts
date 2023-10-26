a2f0fd44dee127142b8db7e32b3621d7d8250ef6
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-26 00:33:01 -0300

    feat: implements generic error handling

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,23 @@ import fastify from 'fastify'
 import 'reflect-metadata'
 import { container } from 'tsyringe'
 import { UsersRoutes } from './http/controllers/users/routes'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 const usersRoutes = container.resolve(UsersRoutes)
 
-app.register(usersRoutes.mountRoutes, { prefix: '/users' })
+app.register((instance) => usersRoutes.mountRoutes(instance), {
+  prefix: '/users',
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  console.error(error.message)
+  return reply.status(500).send({ message: 'Internal server error' })
+})
