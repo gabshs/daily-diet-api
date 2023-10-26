cc6ec1783419067e0bb81bbd89e97d567c864316
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-26 00:16:39 -0300

    feat: implements users routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import fastify, { FastifyReply } from 'fastify'
+import fastify from 'fastify'
+import 'reflect-metadata'
+import { container } from 'tsyringe'
+import { UsersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
-app.get('/', (_, reply: FastifyReply) => {
-  return reply.send('Hello World')
-})
+const usersRoutes = container.resolve(UsersRoutes)
+
+app.register(usersRoutes.mountRoutes, { prefix: '/users' })
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,14 +1,17 @@
 import { FastifyInstance } from 'fastify'
+import { inject, singleton } from 'tsyringe'
 import { RegisterUserController } from './register'
-import { singleton } from 'tsyringe'
 
 @singleton()
 export class UsersRoutes {
   constructor(
+    @inject(RegisterUserController)
     private readonly registerUserController: RegisterUserController,
   ) {}
 
   async mountRoutes(app: FastifyInstance) {
-    app.post('/', this.registerUserController.handle)
+    app.post('/', (request, reply) =>
+      this.registerUserController.handle(request, reply),
+    )
   }
 }
