ac91146c580ba95353883add7b6d25e8643f3518
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-26 02:05:03 -0300

    refact: receives a meal to update meal instead of meal update input

--- src/repositories/meals-repository.ts
+++ src/repositories/meals-repository.ts
@@ -2,7 +2,7 @@ import { Meal, Prisma } from '@prisma/client'
 
 export interface MealsRepository {
   create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
-  update(data: Prisma.MealUpdateInput): Promise<Meal>
+  update(data: Meal): Promise<Meal>
   delete(mealId: string): Promise<void>
   findOneByUser(id: string, userId: string): Promise<Meal | null>
   findManyByUser(userId: string, page: number): Promise<Meal[]>
