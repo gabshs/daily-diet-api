c3c0cae6023bcd393fedb22ee72d4527d58e55a0
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-26 01:43:48 -0300

    feat: implements create meals use case

---
+++ src/use-cases/meals/register.ts
@@ -0,0 +1,31 @@
+import { MealsRepository } from '@/repositories/meals-repository'
+
+interface RegisterMealUseCaseParams {
+  name: string
+  description: string
+  eatenAt: string
+  isOnDiet: boolean
+  userId: string
+}
+
+export class RegisterMealUseCase {
+  constructor(private readonly mealsRepository: MealsRepository) {}
+
+  async execute({
+    name,
+    description,
+    eatenAt,
+    isOnDiet,
+    userId,
+  }: RegisterMealUseCaseParams) {
+    const meal = await this.mealsRepository.create({
+      name,
+      description,
+      eaten_at: eatenAt,
+      is_on_diet: isOnDiet,
+      user_id: userId,
+    })
+
+    return { meal }
+  }
+}
