81024c405b9227348885c895c3bd0d53293d3f21
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 12:25:23 -0300

    feat: register prisma meals repository as singleton

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,3 +1,5 @@
+import { MealsRepository } from '@/repositories/meals-repository'
+import { PrismaMealsRepository } from '@/repositories/prisma/prisma-meals-repository'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UsersRepository } from '@/repositories/users-repository'
 import { container } from 'tsyringe'
@@ -5,6 +7,11 @@ import { container } from 'tsyringe'
 container.registerSingleton<UsersRepository>(
   'UsersRepository',
   PrismaUsersRepository,
+)
+
+container.registerSingleton<MealsRepository>(
+  'MealsRepository',
+  PrismaMealsRepository,
 )
 
 export { container }
