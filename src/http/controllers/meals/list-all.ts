0819aa1ac32877cdec150e63e0b34269d86047e1
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 12:33:07 -0300

    feat: creates list meal controller and its route

---
+++ src/http/controllers/meals/list-all.ts
@@ -0,0 +1,25 @@
+import { ListMealsByUserUseCase } from '@/use-cases/meals/list-by-user'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { inject, singleton } from 'tsyringe'
+import { z } from 'zod'
+
+const listMealsQuerySchema = z.object({
+  page: z.coerce.number().default(1),
+})
+
+@singleton()
+export class ListMealsController {
+  constructor(
+    @inject(ListMealsByUserUseCase)
+    private readonly listMealsUseCase: ListMealsByUserUseCase,
+  ) {}
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { sub } = request.user
+    const { page } = listMealsQuerySchema.parse(request.query)
+
+    const { meals } = await this.listMealsUseCase.execute({ userId: sub, page })
+
+    return reply.status(200).send({ meals })
+  }
+}
--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -2,12 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { inject, singleton } from 'tsyringe'
 import { RegisterMealController } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { ListMealsController } from './list-all'
 
 @singleton()
 export class MealsRoutes {
   constructor(
     @inject(RegisterMealController)
     private readonly registerMealController: RegisterMealController,
+    @inject(ListMealsController)
+    private readonly listMealsController: ListMealsController,
   ) {}
 
   async mountRoutes(app: FastifyInstance) {
@@ -15,6 +18,10 @@ export class MealsRoutes {
 
     app.post('/', (request, reply) =>
       this.registerMealController.handle(request, reply),
+    )
+
+    app.get('/', (request, reply) =>
+      this.listMealsController.handle(request, reply),
     )
   }
 }
