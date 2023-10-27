f34d3c48a0237851c29731f8ff5366046b77fd76
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 15:49:27 -0300

    feat: create, validate and use envs to project

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import { ZodError } from 'zod'
 import fastifyJwt from '@fastify/jwt'
 import { container } from './di/container'
 import { MealsRoutes } from './http/controllers/meals/routes'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
-  secret: 'dailyDietJWTToken',
+  secret: env.JWT_SECRET,
   sign: { expiresIn: '1d' },
 })
 
@@ -29,6 +30,11 @@ app.setErrorHandler((error, _, reply) => {
       .send({ message: 'Validation error', issues: error.format() })
   }
 
-  console.error(error.message)
+  if (env.NODE_ENV !== 'production') {
+    console.error(error.message)
+  } else {
+    // TODO: Here we should log to an external tool
+  }
+
   return reply.status(500).send({ message: 'Internal server error' })
 })
