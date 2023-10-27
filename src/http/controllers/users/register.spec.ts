f6b9bf7edd755ff6ad5afee08e09a70e205e3ae2
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 14:48:08 -0300

    test: creates tests e2e to register user flow

---
+++ src/http/controllers/users/register.spec.ts
@@ -0,0 +1,22 @@
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+
+describe('Register user (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123123q',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
