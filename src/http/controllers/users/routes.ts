db6e7c453e5f8e350b4d4054af41a49d95428f84
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 12:04:12 -0300

    feat: creates route to authenticate user

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,17 +1,24 @@
 import { FastifyInstance } from 'fastify'
 import { inject, singleton } from 'tsyringe'
 import { RegisterUserController } from './register'
+import { AuthenticateController } from './authenticate'
 
 @singleton()
 export class UsersRoutes {
   constructor(
     @inject(RegisterUserController)
     private readonly registerUserController: RegisterUserController,
+    @inject(AuthenticateController)
+    private readonly authenticateUserController: AuthenticateController,
   ) {}
 
   async mountRoutes(app: FastifyInstance) {
     app.post('/', (request, reply) =>
       this.registerUserController.handle(request, reply),
+    )
+
+    app.post('/authenticate', (request, reply) =>
+      this.authenticateUserController.handle(request, reply),
     )
   }
 }
