305a5ee467efb9d161c6f29d02ead55be0123edd
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-10-27 12:33:21 -0300

    feat: adds di to list meals use case

--- src/use-cases/meals/list-by-user.ts
+++ src/use-cases/meals/list-by-user.ts
@@ -1,12 +1,17 @@
 import { MealsRepository } from '@/repositories/meals-repository'
+import { inject, injectable } from 'tsyringe'
 
 interface ListMealsByUserUseCaseParams {
   userId: string
   page: number
 }
 
+@injectable()
 export class ListMealsByUserUseCase {
-  constructor(private readonly mealsRepository: MealsRepository) {}
+  constructor(
+    @inject('MealsRepository')
+    private readonly mealsRepository: MealsRepository,
+  ) {}
 
   async execute({ userId, page }: ListMealsByUserUseCaseParams) {
     const meals = await this.mealsRepository.findManyByUser(userId, page)
